25b462dadf3386dab31714efb916eb97569f9c6a
awill-a-way <will7162@umn.edu>
2025-02-28 02:15:14 -0600

    Slightly changed bodfish 1 (deleted blank line at top) finished bodfish 2

--- bodfish-2 formatting.R
+++ bodfish-2 formatting.R
@@ -5,10 +5,39 @@ library(readxl)
 library(stringr)
 
 # LOAD BODFISH DATA 2
-bodfish_two <- read_xlsx("Ziploc_Experiment_Field_Data_Sheets_Bodfish-2013.xlsx",skip =1)
+bodfish_two <- read_xlsx("Ziploc Experiment Field Data Sheets_ Bodfish_2013.xlsx",skip =1)
 colnames(bodfish_two) <- str_replace(string = colnames(bodfish_two),pattern = "1st", replacement =  "first" )
 bodfish_two <-   clean_names(bodfish_two)
 
 ###############
 # FORMAT BODFISH
 ###############
+
+
+#feb 2014 two
+bodfish_Feb14two <-bodfish_two|>
+  select(c(1:16))|>
+  mutate(year = 2014,
+         month = "feb")
+colnames(bodfish_Feb14two) <- str_remove(string = colnames(bodfish_Feb14two), "_\\d.")
+#mayjun 2014 two (note that 17:25 is uncertain)
+bodfish_mayjun14two <-bodfish_two|>
+  select(c(1:10, 17:25))|>
+  mutate(year = 2014,
+         month = "mayjun")
+colnames(bodfish_mayjun14two) <- str_remove(string = colnames(bodfish_mayjun14two), "_\\d.")
+#feb 2015 two
+bodfish_Feb15two <-bodfish_two|>
+  select(c(1:10, 26:31))|>
+  mutate(year = 2015,
+         month = "feb")
+colnames(bodfish_Feb15two) <- str_remove(string = colnames(bodfish_Feb15two), "_\\d.")
+#mayjun 2015 two
+bodfish_mayjun15two <-bodfish_two|>
+  select(c(1:10, 32:40))|>
+  mutate(year = 2015,
+         month = "mayjun")
+colnames(bodfish_mayjun15two) <- str_remove(string = colnames(bodfish_mayjun15two), "_\\d.")
+
+#Unified bodfish 2014+2015 two
+bind_rows(bodfish_Feb14two,bodfish_mayjun14two,bodfish_Feb15two,bodfish_mayjun15two)
