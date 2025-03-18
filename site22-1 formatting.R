ef24ee4300db178be7bda296d25cf8a4b9084798
awill-a-way <will7162@umn.edu>
2025-03-18 13:05:29 -0500

    Finished formatting site22-1 data

--- site22-1 formatting.R
+++ site22-1 formatting.R
@@ -21,13 +21,40 @@ site22_Feb12 <- site22_original|>
 colnames(site22_Feb12) <- str_remove(string = colnames(site22_Feb12), "_\\d." )
 #Feb13
 site22_Feb13 <- site22_original|>
-  select(18:23)|>
+  select(1:11, 18:23)|>
   mutate(year = 2013,
          month = "feb")
 colnames(site22_Feb13) <- str_remove(string = colnames(site22_Feb13), "_\\d." )
 #March13
-site22_mar13 <- site22_original|>
-  select(24:31)|>
+site22_Mar13 <- site22_original|>
+  select(1:11, 24:31)|>
   mutate(year = 2013,
          month = "mar")
-colnames(site22_mar13) <- str_remove(string = colnames(site22_mar13), "_\\d." )
+colnames(site22_Mar13) <- str_remove(string = colnames(site22_Mar13), "_\\d." )
+#MayJun13
+site22_MayJun13 <- site22_original|>
+  select(1:11, 32:39)|>
+  mutate(year = 2013,
+         month = "mayjun")
+colnames(site22_MayJun13) <- str_remove(string = colnames(site22_MayJun13), "_\\d." )
+#MayJun14
+site22_MayJun14 <- site22_original|>
+  select(1:11, 69:77)|>
+  mutate(year = 2014,
+         month = "mayjun")
+colnames(site22_MayJun14) <- str_remove(string = colnames(site22_MayJun14), "_\\d." )
+#The last column in MayJun14 is a note column BUT it is technically not under May/June Survey
+#Feb15
+site22_Feb15 <- site22_original|>
+  select(1:11, 78:83)|>
+  mutate(year = 2015,
+         month = "feb")
+colnames(site22_Feb15) <- str_remove(string = colnames(site22_Feb15), "_\\d." )
+#MayJun15
+site22_MayJun15 <- site22_original|>
+  select(1:11, 84:92)|>
+  mutate(year = 2015,
+         month = "mayjun")
+colnames(site22_MayJun15) <- str_remove(string = colnames(site22_MayJun15), "_\\d." )
+#Unified site22-1 data
+unified_site22_one <- bind_rows(site22_Feb12, site22_Feb13, site22_Mar13, site22_MayJun13, site22_MayJun14, site22_Feb15, site22_MayJun15)
