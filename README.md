a505f1e32c9594e84c7597756f67bbd342c2dcd1
awill-a-way <will7162@umn.edu>
2025-03-21 13:12:06 -0500

    Changes made for preparing to plot

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # clarkia-data
 #I edited this file on 2/12/2025 at 5:48 PM
+#3/18 - Focus on making a graph and summary stats. next week, be able to point at patterns (specifically with germination)
+#barplots for germinants?
---
+++ clarkia-plotting-practice.R
@@ -0,0 +1,11 @@
+#example
+ggplot(ril_data, aes(x=prop_hybrid))+
+  geom_bar()
+#
+ggplot(ril_data, aes(x=prop_hybrid))+
+  geom_bar()
+#first unifying data
+bind_row(uni)
+#
+ggplot(unified_clarkia, aes(x=germ))+
+  geom_bar()
